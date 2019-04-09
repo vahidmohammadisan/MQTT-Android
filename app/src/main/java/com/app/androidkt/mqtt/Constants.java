358b92500c457043226bb3a7aa378675a97c7e8c
v.mohammadi <optimcomm@gmail.com>
2019-04-09 16:25:39 +0330

    commit

---
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,12 @@
+package com.app.androidkt.mqtt;
+
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://m16.cloudmqtt.com:11366";
+
+    public static final String PUBLISH_TOPIC = "vahid/t1";
+
+    public static final String CLIENT_ID = "a436cfeb-007d-4ec0-89fe-7f6e0a0e8d03";
+}
+
